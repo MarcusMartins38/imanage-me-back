25138750636d50187304db1e3d70d78890d9a09d
unknown <marcusmartins38@gmail.com>
2024-12-12 17:12:54 -0400

    feat: sign-in with jwt token

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,6 +1,7 @@
 import { PrismaClient } from "@prisma/client";
 import express, { Request, Response } from "express";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 const router = express.Router();
 const prisma = new PrismaClient();
@@ -48,6 +49,39 @@ router.post("/sign-up", async (req: Request, res: Response) => {
     }
 });
 
-router.post("/login", async (req: Request, res: Response) => {});
+router.post("/sign-in", async (req: Request, res: Response) => {
+    try {
+        const { email, password } = req.body;
+        if (!email || !password) {
+            return res
+                .status(400)
+                .json({ error: "Email e senha são obrigatórios." });
+        }
+
+        const user = await prisma.user.findUnique({ where: { email } });
+        if (!user) {
+            return res.status(404).json({ error: "Usuário não encontrado." });
+        }
+
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+        if (!isPasswordValid) {
+            return res.status(401).json({ error: "Credenciais inválidas." });
+        }
+
+        const token = jwt.sign(
+            { id: user.id, email: user.email, role: user.role },
+            process.env.JWT_SECRET as string,
+            { expiresIn: process.env.JWT_EXPIRATION || "1h" },
+        );
+
+        return res.status(200).json({
+            message: "Login bem-sucedido.",
+            token,
+        });
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ error: "Erro interno do servidor." });
+    }
+});
 
 export default router;
